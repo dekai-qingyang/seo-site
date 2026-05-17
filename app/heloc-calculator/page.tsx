d07eb824083c013863bb4569b66765917782675b
dekai-qingyang <liuzhijunqaz@outlook.com>
2026-05-18 02:01:54 +0800

    update calculators

--- app/heloc-calculator/page.tsx
+++ app/heloc-calculator/page.tsx
@@ -314,7 +314,7 @@ function InputField({
           onChange={(e) =>
             setValue(Number(e.target.value))
           }
-          className="w-full border rounded-2xl py-3 px-4"
+          className="w-full border rounded-2xl py-3 px-7"
         />
 
         {suffix && (
